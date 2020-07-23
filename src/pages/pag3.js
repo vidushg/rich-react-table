0fee3a8982a68c5bbb41b49f14cf0625174627ab
vidushg <goswamiv@grinnell.edu>
2020-07-22 22:41:13 -0500

    working tooltip for cardName

--- src/pages/pag3.js
+++ src/pages/pag3.js
@@ -1,19 +1,42 @@
 import { Link } from "gatsby"
-  import React, { Component } from "react";
+  import React, { Component, useState, useRef } from "react";
 //import { Table } from "reactstrap";
-
+import "bootstrap/dist/css/bootstrap.css";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import JSONData from "../../content/data.json"
-import { Container, Table} from 'react-bootstrap'
+import {Button, Container, OverlayTrigger, Tooltip, Table} from 'react-bootstrap'
 
 
 const pag3 = () => {
 
+function renderToolTip(props) {
+ return (
+ 	<Tooltip id="button-tooltip" {...props}>
+ 	Tooooltip
+ 	</Tooltip>
+ 	);
+ 
+}
+
+function renderCardNameToolTip(val){
+ return (
+ 	  <Tooltip id="button-tooltip">	
+ 	  <h3 Traits />
+ 	  {val.traits.map(val => (<ul><li>{val.traitname}</li></ul>))}
+ 	  </Tooltip>
+	);
+}
+
 var state = JSONData;
  
- const element = <div id='root'>  <Table striped bordered hover className="align-items-center table-flush" responsive>
+  
+ const element = <div id='root'>
+ 	<OverlayTrigger placement="right" overlay = {renderToolTip}>
+ 	 <Button>HERE</Button>
+ 	 </OverlayTrigger>
+ 	<Table striped bordered hover className="align-items-center table-flush" responsive>
         <thead className="thead-light">
           <tr>
             <th scope="col">Name</th>
@@ -26,7 +49,12 @@ var state = JSONData;
         <tbody>
           {state.content.map(info => (
             <tr>
-              <td scope="row">{info.properties.name}</td>
+              <td scope="row">
+              <OverlayTrigger placement="right" overlay = {renderCardNameToolTip(info.properties)}>
+ 	 <Button>
+              
+              {info.properties.name}</Button>
+              </OverlayTrigger></td>
               <td>{info.properties.cost}</td>
              
                 <td>{info.properties.type}</td>
