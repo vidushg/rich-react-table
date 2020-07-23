1240e4c1b4968c14749d940f1346be2a7bfedf3c
vidushg <goswamiv@grinnell.edu>
2020-07-22 23:08:04 -0500

    added tooltip with traits+icon+ruletext

--- src/pages/pag3.js
+++ src/pages/pag3.js
@@ -1,9 +1,10 @@
 import { Link } from "gatsby"
-  import React, { Component, useState, useRef } from "react";
+  import React from "react";
 //import { Table } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+//import Layout from "../components/layout"
+//import SEO from "../components/seo"
+import "../css/custom.css"
 
 import JSONData from "../../content/data.json"
 import {Button, Container, OverlayTrigger, Tooltip, Table} from 'react-bootstrap'
@@ -11,20 +12,16 @@ import {Button, Container, OverlayTrigger, Tooltip, Table} from 'react-bootstrap
 
 const pag3 = () => {
 
-function renderToolTip(props) {
- return (
- 	<Tooltip id="button-tooltip" {...props}>
- 	Tooooltip
- 	</Tooltip>
- 	);
- 
-}
+/*function to render a tooltip with list of traits, list of icons, and ruletext for a given card*/
 
 function renderCardNameToolTip(val){
  return (
  	  <Tooltip id="button-tooltip">	
- 	  <h3 Traits />
- 	  {val.traits.map(val => (<ul><li>{val.traitname}</li></ul>))}
+ 	  <em> Traits </em>
+ 	  {val.traits.map(val => (<ul><li>{val.traitname}</li></ul>))}<br />
+ 	  <em>Icons</em> <br />
+ 	  {val.testicons.map(val => (<ul><li>{val.iconname}</li></ul>))} <br />
+ 	  <p>{val.ruletext}</p>
  	  </Tooltip>
 	);
 }
@@ -33,35 +30,35 @@ var state = JSONData;
  
   
  const element = <div id='root'>
- 	<OverlayTrigger placement="right" overlay = {renderToolTip}>
- 	 <Button>HERE</Button>
- 	 </OverlayTrigger>
  	<Table striped bordered hover className="align-items-center table-flush" responsive>
         <thead className="thead-light">
           <tr>
             <th scope="col">Name</th>
+            <th scope="col">Class</th>
             <th scope="col">cost</th>
             <th scope="col">Type</th>
             <th scope="col">Set</th>
-            <th scope="col">Traits</th>
           </tr>
         </thead>
         <tbody>
           {state.content.map(info => (
             <tr>
-              <td scope="row">
+              <td>
               <OverlayTrigger placement="right" overlay = {renderCardNameToolTip(info.properties)}>
- 	 <Button>
+ 	 <Button variant="Link">
               
               {info.properties.name}</Button>
               </OverlayTrigger></td>
+              
+              <td><ul class="comma-list">
+              {info.properties.class.map(val => (<li>{val.classname}</li>))}</ul>
+              </td>
+              
               <td>{info.properties.cost}</td>
              
                 <td>{info.properties.type}</td>
               
               <td>{info.properties.set}</td>
-              <td>{info.properties.traits.map(val => (<ul><li>{val.traitname}</li></ul>))}
-              </td>
               
             </tr>
           ))}
